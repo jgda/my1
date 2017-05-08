09cb5d13b8af8f63342779119c4d78fd0f4aa97f
jgda <kevin@junggu-da.com>
2017-05-09 00:16:44 +0900

    fix error in main.php
    remove the warning message

--- main.php
+++ main.php
@@ -8,7 +8,7 @@
 		<title>ProdcutPage</title>
 		<meta name="description" content="">
 		<meta name="author" content="Kevin">
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
 		<link href="/favicon.ico" rel="shortcut icon" />
 		<link href="/apple-touch-icon.png" rel="apple-touch-icon" />
